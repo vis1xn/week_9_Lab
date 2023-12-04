ff9959afa1440760f22b153868233c78308d1767
Tyrese <g00421432@atu.ie>
2023-12-04 10:27:11 +0000

    added branch feature 2

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -3,9 +3,9 @@ package ie.atu;
 public class Main {
     public static void main(String[] args)
     {
-        Person myPerson = new Person("Tyrese", "Athlone", "0899785456");
-        System.out.println(myPerson.toString());
-        Customer myCustomer = new Customer("TJ", "DUBLIN", "987654321", "28105", true);
+        Customer Part1 = new Customer("TJ", "DUBLIN", "987654321", "28105", true);
         System.out.println(myCustomer.toString());
+
+        Customer Part2 = new Customer()
     }
 }
