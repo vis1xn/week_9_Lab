9737b94054197dcda58d7e0d3e28ad7c91e45c9d
Tyrese <g00421432@atu.ie>
2023-12-04 09:40:08 +0000

    added new code to person and main

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -1,7 +1,8 @@
 package ie.atu;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Person myPerson = new Person("Tyrese", "Athlone", "0899785456");
     }
 }
